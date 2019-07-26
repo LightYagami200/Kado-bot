595a8173fbe3ef86fbbd0223e6260120878920fe
Light Yagami <abdulsammad200@gmail.com>
2019-07-26 11:14:04 +0500

    Added voting reward

--- server/routes/webhooks.js
+++ server/routes/webhooks.js
@@ -1,8 +1,19 @@
+//Dependencies
 const { discordBotsAuth } = require('../config/keys');
+const mongoose = require('mongoose');
+
+//Init
+const Profile = mongoose.model('Profile');
 
 module.exports = (app, client) => {
-  app.post('/webhooks', (req, res) => {
-    console.log(req.headers.authorization);
-    console.log(req.body);
+  app.post('/webhooks', async (req, res) => {
+    if (req.headers.authorization !== discordBotsAuth)
+      return res.status(401).json({ err: 'Invalid Authorization Header' });
+
+    const profile = await Profile.findOne({ memberID: req.body.user }).exec();
+
+    if (!profile) return;
+
+    return profile.addCoins(10);
   });
 };
