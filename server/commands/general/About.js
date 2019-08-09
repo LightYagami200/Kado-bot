5089df9b63ceea19b934c27587bc20c8891b6a19
Light Yagami <abdulsammad200@gmail.com>
2019-08-09 09:36:04 +0500

    Fixed about Embed Formatting

--- server/commands/general/About.js
+++ server/commands/general/About.js
@@ -21,7 +21,7 @@ module.exports = class LeaderboardsCommand extends Command {
       new RichEmbed()
         .setTitle('About Kādo Bot')
         .setDescription(
-          'Kādo bot is an Anime card collecting/dueling bot, to invite it to your server, use this link: \n\nhttps://discordapp.com/api/oauth2/authorize?client_id=582271366619725855&permissions=280640&scope=bot\n\nVersion 0.2.1 changelog:\n>Fixed critical bug\n>Fixed guide & about formatting  Version 0.2 changelog:\n> Added long awaited Trading feature\n> Players get 3 cards at the start of the duel (from their main deck) to give more choice to players\n> Added this command (about)\n>Bug fixes'
+          'Kādo bot is an Anime card collecting/dueling bot, to invite it to your server, use this link: \n\nhttps://discordapp.com/api/oauth2/authorize?client_id=582271366619725855&permissions=280640&scope=bot\n\nVersion 0.2.1 changelog:\n> Fixed critical bug\n> Fixed guide & about formatting\n\nVersion 0.2 changelog:\n> Added long awaited Trading feature\n> Players get 3 cards at the start of the duel (from their main deck) to give more choice to players\n> Added this command (about)\n> Bug fixes'
         )
         .setFooter('Version: 0.2.1')
         .setColor('#2196f3')
