8aaff012c9089e94fb13cafd70e539fe51b7b2b7
Light Yagami <abdulsammad200@gmail.com>
2019-08-12 19:13:40 +0500

    updated about command

--- server/commands/general/About.js
+++ server/commands/general/About.js
@@ -21,9 +21,9 @@ module.exports = class LeaderboardsCommand extends Command {
       new RichEmbed()
         .setTitle('About Kādo Bot')
         .setDescription(
-          'Kādo bot is an Anime card collecting/dueling bot, to invite it to your server, use this link: \n\nhttps://discordapp.com/api/oauth2/authorize?client_id=582271366619725855&permissions=280640&scope=bot\n\nVersion 0.2.1 changelog:\n> Fixed critical bug\n> Fixed guide & about formatting\n\nVersion 0.2 changelog:\n> Added long awaited Trading feature\n> Players get 3 cards at the start of the duel (from their main deck) to give more choice to players\n> Added this command (about)\n> Bug fixes'
+          "Kādo bot is an Anime card collecting/dueling bot, to invite it to your server, use this link: \n\nhttps://discordapp.com/api/oauth2/authorize?client_id=582271366619725855&permissions=280640&scope=bot\n\nVersion 0.2.2 changelog\n> Fixed bug where players can't play a card when they've got multiple cards of same name\n\n0.2.1 changelog:\n> Fixed critical bug\n> Fixed guide & about formatting\n\nVersion 0.2 changelog:\n> Added long awaited Trading feature\n> Players get 3 cards at the start of the duel (from their main deck) to give more choice to players\n> Added this command (about)\n> Bug fixes"
         )
-        .setFooter('Version: 0.2.1')
+        .setFooter('Version: 0.2.2')
         .setColor('#2196f3')
     );
   }
