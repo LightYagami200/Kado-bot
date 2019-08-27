35c9f7e3615debb7e26fec9f05fb0f052e779c33
Light Yagami <abdulsammad200@gmail.com>
2019-08-27 09:45:13 +0500

    Updated to 0.3.0.2

--- server/commands/general/About.js
+++ server/commands/general/About.js
@@ -8,7 +8,7 @@ module.exports = class LeaderboardsCommand extends Command {
     super(client, {
       name: 'about',
       guildOnly: false,
-      aliases: ['about'],
+      aliases: ['about', 'info', 'changelog'],
       group: 'general',
       memberName: 'about',
       description: 'About Kado Bot',
@@ -21,7 +21,7 @@ module.exports = class LeaderboardsCommand extends Command {
       new MessageEmbed()
         .setTitle('About Kādo Bot')
         .setDescription(
-          'Kādo bot is an Anime card collecting/dueling bot, to invite it to your server, use this link: \n\nhttps://discordapp.com/api/oauth2/authorize?client_id=582271366619725855&permissions=280640&scope=bot\n\nVersion 0.3.0.1 Changelog\n> Fixed partners command bug\n> Fixed Website CSS\n\nVersion 0.3 Changelog\n> Completely revamped attributes\n> Re-wrote the whole guide (took me whole 2 hours, So I hope it helps you)\n> Redesigned move command to make it user-friendly (dw tho, old command is still here but renamed to moveOld)\n> Major performance improvements\n> Fixed bugs'
+          'Kādo bot is an Anime card collecting/dueling bot, to invite it to your server, use this link: \n\nhttps://discordapp.com/api/oauth2/authorize?client_id=582271366619725855&permissions=280640&scope=bot\n\nVersion 0.3.0.2 Changelog\n> Removed annoying "Unknown Command" message\n> Minor changes\n\nVersion 0.3.0.1 Changelog\n> Fixed partners command bug\n> Fixed Website CSS\n\nVersion 0.3 Changelog\n> Completely revamped attributes\n> Re-wrote the whole guide (took me whole 2 hours, So I hope it helps you)\n> Redesigned move command to make it user-friendly (dw tho, old command is still here but renamed to moveOld)\n> Major performance improvements\n> Fixed bugs'
         )
         .setFooter('Version: 0.3')
         .setColor('#2196f3')
